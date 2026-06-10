4aee48a65eee13795ddf12f4b84cf630b675287e
Vishal Amol Golwad <vishalgolwad17@gmail.com>
2026-06-10 16:40:56 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Navbar >
     <App />
+    <Analytics />
     </Navbar>
   </BrowserRouter>
 )
